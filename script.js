29f89eb3b048f6bbbca04183657bb3342509d3fc
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-09-19 19:14:01 +0100

    update 'operatorPressed' function to clear numberDisplay before beginning new calculation

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 let numberDisplay = document.getElementById('number-display');
 numberDisplay.textContent = '';
+let equals = '';
 
 const numButtons = document.querySelectorAll('.num-button');
 numButtons.forEach(num => {
-	num.addEventListener('click', () => {numberDisplay.textContent += num.textContent});
+	num.addEventListener('click', () => {
+		if (equals == 'pressed') {
+			numberDisplay.textContent = '';
+			numberDisplay.textContent += num.textContent;
+			equals = '';
+		} else {
+			numberDisplay.textContent += num.textContent
+		}
+	});
 });
 
 const operatorButtons = document.querySelectorAll('.operator-button');
 
 function operatorPressed(e) {
-	
+	console.log(e);
 	if(e.target.id === 'equals') {
 		num2 = numberDisplay.textContent;
 		numberDisplay.textContent = '';
 		//calls function & converts strings to integers and string to ...
 		solution = operate(+num1, window[operator], +num2);
 		numberDisplay.textContent = solution;
+		equals = 'pressed';
 		
 	} else {
 		operator = e.target.id;
@@ -69,13 +79,21 @@ function operate(num1, operator, num2) {
 
 //adding keyboard function to it. Using Wes Bos' DrumKit tut for this!:
 window.addEventListener('keydown', function(e) {
+	// console.log(e.key);
 	const numPress = document.querySelector(`button[class~='num-button'][data-key='${e.key}']`);
 	const operatorPress = document.querySelector(`button[class~='operator-button'][data-key='${e.key}']`);
-	if(!numPress) return; 
-	numberDisplay.textContent += numPress.textContent;
+	
+	if(numPress) {
+		numberDisplay.textContent += numPress.textContent;
+	} else if(operatorPress) {
+		operatorPressed(operatorPress);
+	} else {return;
+	}
 });
-//NEED TO MATCH KEYCODE TO DATA-KEY??? No: //.which and .keyCode have both been deprecated. 
-//Use .key now, which returns a string //that matches the key pressed. 
+
+//use the keycode rather than text or symbols for the operators
+//NEED TO MATCH KEYCODE TO DATA-KEY??? No: .which and .keyCode have both been deprecated. 
+//Use .key now, which returns a string that matches the key pressed. 
 //Pressing the 'a' key returns 'a', 'B' returns 'B', '1' returns '1', etc. 
 //It means there's no need to find a keycode anymore as the .key value
 //is literally the same as the key being pressed/listened for!
@@ -85,3 +103,6 @@ window.addEventListener('keydown', function(e) {
 //if e !== data-key return
 //else input.textContent = e.textContent
 //???
+
+//need to reset display to no content after answer calculated before next 
+//calculation entered.
